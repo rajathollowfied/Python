ff84ff4c50c2eab46c3a33c2cd76b115691ce8d2
Rajat <rajat.hollowfied@gmail.com>
2022-07-14 02:59:56 +0530

    importing data

--- first_project/classes/class_pyspark.py
+++ first_project/classes/class_pyspark.py
@@ -7,17 +7,18 @@ from pyspark import SparkContext
 
 class Sparkclass:
 
-    def __init__(self, strdict):
+    def __init__(self, strdict):    #blank dict is passed from sales job
         self.strdict = strdict
     
     def sparkStart(self, kwargs:dict):
         MASTER = kwargs['spark_conf']['master']
         APP_NAME = kwargs['spark_conf']['app_name']
-        LOG_LEVEL = kwargs['log']['level']
+        #LOG_LEVEL = kwargs['log']['level']
         
         def createSession(master:Optional[str]="local[*]",app_name:Optional[str]="myapp"):
             #creating a spark session
             spark = SparkSession.builder.appName(app_name).master(master).getOrCreate()
+            print(f"Session Created !! - {spark}")
             return spark
 
         def setLogging():
@@ -25,10 +26,20 @@ class Sparkclass:
 
         def getSettings(spark:SparkSession) -> None:
             #show spark settings
-            print(spark)
+            
             print(spark.sparkContext.getConf().getAll())
             
         spark = createSession(MASTER, APP_NAME)
-        getSettings(spark)
+        #getSettings(spark)
 
         return spark
+
+    def importData(self, spark:SparkSession, datapath:str, pattern:Optional[str]=None) -> DataFrame:
+        
+        def fileOrDirectory(datapath:str) -> str:
+            if isinstance(datapath,str) and os.path.exists(datapath):
+                if os.path.isdir(datapath):
+                    return "dir"
+                elif os.path.isfile(datapath):
+                    return "file"
+        pathType = fileOrDirectory(datapath)
--- first_project/jobs/sales.py
+++ first_project/jobs/sales.py
@@ -6,23 +6,31 @@ from pyspark import SparkStageInfo
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql import SparkSession
 
+
 # setting up environment variables and logger information
 proj_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 job_name = os.path.basename(__file__)
-LOG_FILE = f"{proj_dir}/logs/job-{job_name}.log"
-LOG_FORMAT = f"%(asctime)s - LINE:%(lineno)d - %(name)s - %(levelname)s - %(funcName)s - %(message)s"
-logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG, format=LOG_FORMAT)
-logger = logging.getLogger('py4j')
+#LOG_FILE = f"{proj_dir}/logs/job-{job_name}.log"
+#LOG_FORMAT = f"%(asctime)s - LINE:%(lineno)d - %(name)s - %(levelname)s - %(funcName)s - %(message)s"
+#logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG, format=LOG_FORMAT)
+#logger = logging.getLogger('py4j')
 
 # including class directory containing custom modules
 sys.path.insert(1, proj_dir)
 from classes import class_pyspark
 
 def main(proj_dir:str) -> None:
+    
     #calling function to open and read the file(s) and return contents as dict type
-    config = openFile(f"{proj_dir}/config_json/sales.json")
+    jsonConfig = openFile(f"{proj_dir}/config_json/sales.json")
+    
     #starts the spark job
-    start = sparkStart(config)
+    start = sparkStart(jsonConfig)
+    
+    #import json files from the specified directory
+    transactionsDf = importData(start, f"{proj_dir}/test_data/sales/transactions", ".json$")
+    
+
     #stop the spark job
     sparkStop(start)
     
@@ -34,13 +42,17 @@ def openFile(jsonFile:str) -> dict:
             return data
     return (openJson(jsonFile))
 
-def sparkStart(config:dict) -> SparkSession:
-    if isinstance(config,dict):
-        class_pyspark.Sparkclass(strdict={}).sparkStart(config)
+def sparkStart(jsonConfig:dict) -> SparkSession:
+    if isinstance(jsonConfig,dict):
+         return class_pyspark.Sparkclass(strdict={}).sparkStart(jsonConfig)
        
-def sparkStop(spark:SparkSession) -> None:
-    if isinstance(spark,SparkSession):
-        spark.stop()    
+def sparkStop(start:SparkSession) -> None:
+    if isinstance(start,SparkSession):
+        start.stop() #pre-built stop function in SparkSession   
+
+def importData(start:SparkSession, datapath:str, pattern:Optional[str]=None) -> DataFrame:
+    if isinstance(start, SparkSession):
+        return class_pyspark.Sparkclass(strdict={}).importData(start, datapath, pattern)
 
 if __name__ == '__main__':
     main(proj_dir)
